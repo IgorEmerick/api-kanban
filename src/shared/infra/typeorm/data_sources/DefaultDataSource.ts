396be314d385232e1eb4856cc37ea6b9a52368da
Igor Emerick <igorbarbosaemerick@gmail.com>
2023-02-13 20:13:27 -0300

    config: add typeorm default config

---
+++ src/shared/infra/typeorm/data_sources/DefaultDataSource.ts
@@ -0,0 +1,17 @@
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+
+config();
+
+export const DefaultDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: 'kanban_board',
+  schema: 'public',
+  synchronize: false,
+  entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
+  migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+});
